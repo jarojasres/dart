dc61873ddee4f129a78731c5937724ad6e0a4d55
julian.rojas <jarojasres@gmail.com>
2021-09-13 23:01:22 -0500

    Add comments

---
+++ comments.dart
@@ -0,0 +1,12 @@
+main() {
+  //Single line.
+  greetings(1, "Hello!");
+}
+
+// Comments for documentation.
+/// Greet function.
+///
+/// Parameters [code] and [message].
+greetings(int code, String message) {
+  print('${code}-${message}');
+}
