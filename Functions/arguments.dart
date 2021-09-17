ad14cfdd48207488698b1912c40a8ecb106afc2c
julian.rojas <jarojasres@gmail.com>
2021-09-16 23:41:19 -0500

    Update function arguments

--- Functions/arguments.dart
+++ Functions/arguments.dart
@@ -10,7 +10,9 @@ void Greeting(String message, [String name = 'default Value', int age = 20]) {
   print('$message $name - $age');
 }
 
+// the parameters inside the braces are arguments by name.
+// If I need it to be required, but without a default value the word required is placed.
 void Greeting2(String mensaje, {required String nombre, int veces = 10}) {
-  // cuerpo de la función
+  // Body function
   print('Saludar2: $mensaje $nombre - $veces');
 }
